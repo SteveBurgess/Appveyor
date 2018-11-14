961aa0aa19aa2b9fa0b150f7d1785fcd42428320
Stephen Burgess <stephen_burgess@hotmail.com>
2018-11-14 19:48:39 +0000

    First Commit

---
+++ AppveyorTest/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppveyorTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var assemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var version = System.Diagnostics.FileVersionInfo.GetVersionInfo(assemblyLocation).ProductVersion;
+            //Console.WriteLine(); // -> "1.2.32-alpha-01"
+            string test = "test";
+            MessageBox.Show(version);
+        }
+    }
+}
